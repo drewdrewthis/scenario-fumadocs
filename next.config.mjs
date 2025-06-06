125ec097995095cee89c226c3909fe525742cb87
drewdrew <drewdrewthis@gmail.com>
2025-06-06 16:18:54 +0200

    Set up static deploy ghpages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
+  output: "export", // Enable static export
+  // basePath: process.env.NODE_ENV === "production" ? "/scenario-fumadocs" : "", // Add your repo name as basePath
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default withMDX(config);
